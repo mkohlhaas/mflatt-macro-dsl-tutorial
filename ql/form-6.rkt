0bf7ba25f9ca56ff15e29444ab1ef4b0e1b25729
Hans-J. Schmid <knock@myopendoor.de>
2020-06-16 02:21:43 +0200

    finishes standard form macros.

---
+++ ql/form-6.rkt
@@ -0,0 +1,49 @@
+#lang racket
+
+(require "gui.rkt"
+         "ops.rkt"
+         syntax/parse/define)
+
+(provide form
+         (rename-out [boolean-widget boolean]
+                     [money-widget money]
+                     [-/coerce -]
+                     [>/coerce >]
+                     [</coerce <]
+                     [=/coerce =]
+                     [and/coerce and]
+                     [or/coerce or])
+         when
+         #%app
+         #%datum
+         #%module-begin)
+
+(define-syntax-parser form
+  ([_ form-name:id clause ...]
+   #'(begin
+       (define form-name (make-gui 'form-name))
+       (form-clause form-name #t clause) ...
+       (send form-name start))))
+
+(define-syntax-parser form-clause
+  #:literals (when)
+  [(_ form-name guard-expr (when cond-expr clause ...))
+   #'(begin
+       ; Nested when's are not working:
+       ; (define (new-guard-expr) (and guard-expr (?/coerce cond-expr)))
+       (form-clause form-name (and guard-expr (?/coerce cond-expr)) clause ) ...)]
+  [(_ form-name guard-expr [local-id label widget-type])
+   #'(form-clause* form-name guard-expr [local-id label widget-type #f])]
+  [(_ form-name guard-expr [local-id label widget-type calculated-field-fn-or-false])
+   #'(form-clause* form-name guard-expr [local-id label widget-type (lambda() calculated-field-fn-or-false)])])
+
+(define-syntax-parser form-clause*
+  [(_ form-name guard-expr [local-id:id label:str widget-type calculated-field-fn-or-false])
+   #'(begin
+       (define local-id undefined)
+       (gui-add! form-name
+                 widget-type
+                 label
+                 (lambda() guard-expr)
+                 (lambda (v) (set! local-id v))
+                 calculated-field-fn-or-false))])
--- ql/house-7.rkt
+++ ql/house-7.rkt
@@ -8,3 +8,12 @@
         [valueResidue "Value residue:" money
                       ;; Using `when` here should not work:
                       (- sellingPrice privateDebt)]))
+
+#;
+(form Box1HouseOwning
+      [hasSoldHouse "Did you sell a house in 2010?" boolean]
+      (when hasSoldHouse
+        [sellingPrice "Price the house was sold for:" money]
+        [privateDebt "Private debts for the sold house:" money]
+        [valueResidue "Value residue:" money (when #t
+                      (- sellingPrice privateDebt))]))
